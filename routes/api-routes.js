7231c283530f2ab8b30a8ad0f6dc60449d87c5c1
Rocky <rrsalerno21@gmail.com>
2020-07-10 15:55:46 -0700

    added GET route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -36,18 +36,19 @@ module.exports = function(app) {
     res.redirect("/");
   });
 
-  // Route for getting some data about our user to be used client side
+  // Route for getting data about our user's tasks to be used client side
   app.get("/api/user_data", (req, res) => {
     if (!req.user) {
       // The user is not logged in, send back an empty object
-      res.json({});
+      res.status(403).end();
     } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
+      // Otherwise send back the user's data
+      db.Task.findAll({
+        where: {
+          userID: req.user.id
+        },
+        include: db.User.email
+      }).then(result => res.send(result));
     }
   });
 };
