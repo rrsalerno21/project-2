1fc2beff96f6f25937f555f6aa9d8d7dedf17f99
mrenadams <marissaa@mendwell.com>
2020-07-14 12:59:10 -0700

    styled login & signup pg

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,17 +7,17 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    //if (req.user) {
+      //res.redirect("/members");
+    //}
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    //if (req.user) {
+      //res.redirect("/members");
+    //}
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
