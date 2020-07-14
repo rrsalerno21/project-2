4565791f43a8bda11f53bf9d033e25e62f983cbf
Rocky <rrsalerno21@gmail.com>
2020-07-14 11:46:53 -0700

    added complete task event listener

--- public/js/members.js
+++ public/js/members.js
@@ -7,4 +7,34 @@ $(document).ready(() => {
     // below is provided code from init
     $(".member-name").text(data.email);
   });
+
+  $(".complete-task-btn").on("click", function() {
+    let updatedTaskStatus;
+    const selectedRow = $(this)
+      .parent()
+      .parent();
+    const dataId = selectedRow.data("id");
+    const taskComplete = selectedRow.data("complete");
+    const taskDate = selectedRow.children(".task-date").text();
+    const taskTitle = selectedRow.children(".task-title").text();
+    const taskCategory = selectedRow.children(".task-category").text();
+
+    if (taskComplete) {
+      updatedTaskStatus = false;
+    } else {
+      updatedTaskStatus = true;
+    }
+
+    $.ajax({
+      url: "/api/edit",
+      type: "PUT",
+      data: {
+        task: taskTitle,
+        due_date: taskDate,
+        category: taskCategory,
+        complete: updatedTaskStatus,
+        id: dataId
+      }
+    }).then(data => console.log(`${data}`));
+  });
 });
