a0e9238a7585d906a5f689ec32f0d3c96bbd41ca
Rocky <rrsalerno21@gmail.com>
2020-07-14 14:04:50 -0700

    added task render and complete task functionality

--- public/js/members.js
+++ public/js/members.js
@@ -1,14 +1,125 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
-    console.log(data);
+  // Render on load
+  renderTasks();
 
-    // below is provided code from init
-    $(".member-name").text(data.email);
-  });
+  // Event Listeners
+  // eslint-disable-next-line prefer-arrow-callback
+  $(document).on("click", ".complete-task-btn", updateTaskStatus);
 
-  $(".complete-task-btn").on("click", function() {
+  // Function to render the task list
+  function renderTasks() {
+    // identify the tasks div
+    const tasksDiv = $("#tasks-div");
+
+    // first empty the tasksDiv
+    tasksDiv.empty();
+
+    // then get our user's data
+    $.get("/api/user_data").then(data => {
+      console.log(data);
+
+      // then iterate through the data
+      for (item of data) {
+        // identify each item of data
+        const taskId = item.id,
+          taskComplete = item.complete,
+          taskTitle = item.task,
+          taskDate = item.due_date,
+          taskCategory = item.category;
+
+        // create a new row with that data
+        const newRow = createTaskRow(
+          taskId,
+          taskComplete,
+          taskTitle,
+          taskDate,
+          taskCategory
+        );
+
+        // append the new row to our tasksDiv
+        tasksDiv.append(newRow);
+      }
+    });
+  }
+
+  // Function to create new task rows
+  function createTaskRow(
+    taskId,
+    taskComplete,
+    taskTitle,
+    taskDate,
+    taskCategory
+  ) {
+    const rowItem = $("<tr>");
+
+    if (taskComplete) {
+      rowItem.attr({
+        "data-id": taskId,
+        "data-complete": taskComplete,
+        class: "table-success"
+      });
+    } else {
+      rowItem.attr({
+        "data-id": taskId,
+        "data-complete": taskComplete
+      });
+    }
+
+    const formattedTime = moment(taskDate).format("MM/DD");
+
+    const dateItem = $("<td>")
+      .attr({
+        class: "task-date",
+        scope: "row"
+      })
+      .text(formattedTime);
+
+    const titleItem = $("<td>")
+      .attr({
+        class: "task-title"
+      })
+      .text(taskTitle);
+
+    const categoryItem = $("<td>")
+      .attr({
+        class: "task-category"
+      })
+      .text(taskCategory);
+
+    const buttonSection = $("<th>");
+
+    const completeButton = $("<button>")
+      .attr({
+        type: "button",
+        class: "btn btn-success mt-3 complete-task-btn"
+      })
+      // eslint-disable-next-line quotes
+      .html(`<i class="fas fa-check-square"></i>`);
+
+    const deleteButton = $("<button>")
+      .attr({
+        type: "button",
+        class: "btn btn-danger mt-3 delete-task-btn"
+      })
+      // eslint-disable-next-line quotes
+      .html(`<i class="fas fa-trash-alt"></i>`);
+
+    const editButton = $("<button>")
+      .attr({
+        type: "button",
+        class: "btn btn-warning mt-3 edit-task-btn"
+      })
+      // eslint-disable-next-line quotes
+      .html(`<i class="fas fa-pencil-alt"></i>`);
+
+    buttonSection.append(completeButton, deleteButton, editButton);
+    rowItem.append(dateItem, titleItem, categoryItem, buttonSection);
+
+    return rowItem;
+  }
+
+  // Function to update the status of the task
+  function updateTaskStatus() {
     let updatedTaskStatus;
     const selectedRow = $(this)
       .parent()
@@ -18,6 +129,8 @@ $(document).ready(() => {
     const taskDate = selectedRow.children(".task-date").text();
     const taskTitle = selectedRow.children(".task-title").text();
     const taskCategory = selectedRow.children(".task-category").text();
+
+    selectedRow.toggleClass("table-success");
 
     if (taskComplete) {
       updatedTaskStatus = false;
@@ -36,5 +149,5 @@ $(document).ready(() => {
         id: dataId
       }
     }).then(data => console.log(`${data}`));
-  });
+  }
 });
