f5bc7bfdc8d5d0baa4bc6422405186fa8a01c952
Rocky <rrsalerno21@gmail.com>
2020-07-15 10:40:13 -0700

    added 'state' input to FE and BE js for routing

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,30 +3,38 @@ $(document).ready(() => {
   const signUpForm = $("form.signup");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const stateInput = $("#state");
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // When the signup button is clicked, we validate the email and password and state are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
+    
     const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      state: stateInput.val()
     };
 
-    if (!userData.email || !userData.password) {
+    
+
+    if (!userData.email || !userData.password || !userData.state) {
+      alert("Please provide an email address, password, and state location")
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.password, userData.state);
     emailInput.val("");
     passwordInput.val("");
+    stateInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, password, state) {
     $.post("/api/signup", {
       email: email,
-      password: password
+      password: password,
+      state: state
     })
       .then(() => {
         window.location.replace("/members");
@@ -36,6 +44,7 @@ $(document).ready(() => {
   }
 
   function handleLoginErr(err) {
+    console.log(err);
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
