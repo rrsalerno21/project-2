5623991efb08cd1dd9750c36a2af85d002d715d1
Rocky <rrsalerno21@gmail.com>
2020-07-16 13:21:16 -0700

    fixed config setup and reference in index.js

--- config/config.js
+++ config/config.js
@@ -1,29 +1,23 @@
 //const envVar =
 require("dotenv").config();
 
-const setConfig = () => {
-  const config = {
-    development: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: "mysql"
-    },
-    test: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: "mysql"
-    },
-    production: {
-      use_env_variable: "JAWSDB_URL",
-      dialect: "mysql"
-    }
-  };
-  console.log("***", config);
-  return JSON.stringify(config);
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
 };
-
-module.exports = setConfig;
