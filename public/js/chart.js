2a915e70d02e16cfca62a6415bc96c47f074385b
Rocky <rrsalerno21@gmail.com>
2020-07-17 13:06:11 -0700

    cleaned up format

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,72 +1,72 @@
-var state = document.getElementById('state');
-var submitbtn = document.querySelector('#search');
-var deletebtn = document.querySelector('#delete')
-let stateRes = [];
-let positiveRes= [];
+const state = document.getElementById("state");
+const submitbtn = document.querySelector("#search");
+const deletebtn = document.querySelector("#delete");
+const stateRes = [];
+const positiveRes = [];
 
+submitbtn.addEventListener("click", () => {
+  const settings = {
+    url: "https://covidtracking.com/api/states?state=" + state.value,
+    method: "GET",
+    "content-type": "applicatiion/json",
+    accept: "application/json",
+    timeout: 0
+  };
 
-submitbtn.addEventListener('click', function(){
-  var settings = {
-  "url": "https://covidtracking.com/api/states?state=" + state.value,
-  "method": "GET",
-  "content-type": "applicatiion/json",
-  "accept": "application/json",
-  "timeout": 0,
-}
-
-    $.ajax(settings).done(function (response) {
-      stateRes.push(response.state)
-      positiveRes.push(response.positive)
-      createChart();
-    });
+  $.ajax(settings).done(response => {
+    stateRes.push(response.state);
+    positiveRes.push(response.positive);
+    createChart();
+  });
 });
 
-deletebtn.addEventListener('click', function (){
-    var settings = {
-        "url": "https://covidtracking.com/api/states?state=" + state.value,
-        "method": "GET",
-        "content-type": "applicatiion/json",
-        "accept": "application/json",
-        "timeout": 0,
-      }
-      
-          $.ajax(settings).done(function (response) {
-            stateRes.pop(response.state)
-            positiveRes.pop(response.positive)
-            createChart();
-          });
+deletebtn.addEventListener("click", () => {
+  const settings = {
+    url: "https://covidtracking.com/api/states?state=" + state.value,
+    method: "GET",
+    "content-type": "applicatiion/json",
+    accept: "application/json",
+    timeout: 0
+  };
+
+  $.ajax(settings).done(response => {
+    stateRes.pop(response.state);
+    positiveRes.pop(response.positive);
+    createChart();
+  });
 });
 
 createChart();
 
 function createChart() {
+  const myChart = document.getElementById("myChart").getContext("2d");
 
-    let myChart = document.getElementById('myChart').getContext('2d');
-
-
-    
-    let massPopChart = new Chart(myChart, {
-      type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-      data:{
-        labels: stateRes,
-        datasets:[{
-          label:'Covid Cases',
+  const massPopChart = new Chart(myChart, {
+    type: "bar", // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+    data: {
+      labels: stateRes,
+      datasets: [
+        {
+          label: "Covid Cases",
           data: positiveRes,
-          
-          backgroundColor:[
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-            'rgba(153, 102, 255, 0.6)',
-            'rgba(255, 159, 64, 0.6)',
-            'rgba(255, 99, 132, 0.6)'
+
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.6)",
+            "rgba(54, 162, 235, 0.6)",
+            "rgba(255, 206, 86, 0.6)",
+            "rgba(75, 192, 192, 0.6)",
+            "rgba(153, 102, 255, 0.6)",
+            "rgba(255, 159, 64, 0.6)",
+            "rgba(255, 99, 132, 0.6)"
           ],
-          borderWidth:2,
-          borderColor:'#777',
-          hoverBorderWidth:3,
-          hoverBorderColor:'#000'
-        }]
-      },
-    });
-};
+          borderWidth: 2,
+          borderColor: "#777",
+          hoverBorderWidth: 3,
+          hoverBorderColor: "#000"
+        }
+      ]
+    }
+  });
+
+  return massPopChart;
+}
